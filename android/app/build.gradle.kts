45e01de3a3fce8d91c9476b875b867cf76ed95f3
Odinachi David <odinachidavid@yahoo.com>
2026-01-02 08:59:48 +0100

    fix: update NDK version, import ffmpeg_kit_flutter_new_min, and bump whisper_ggml dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "app.netlify.gikwegbu.echolang"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "26.1.10909125"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
